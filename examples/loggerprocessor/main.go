47baa68ee43d9a54455671fa16d50c75bff15134
Codey Whitt <codey.whitt@gmail.com>
2019-07-23 22:56:36 -0600

    update the example to use the new API

--- examples/loggerprocessor/main.go
+++ examples/loggerprocessor/main.go
@@ -5,29 +5,51 @@ import (
 	"os/signal"
 
 	"github.com/sinisterminister/coinfactory"
-	"github.com/sinisterminister/coinfactory/pkg/binance"
 	log "github.com/sirupsen/logrus"
 )
 
-// LoggerSymbolTickerStreamProcessor implements the SymbolStreamProcessor interface.
-type LoggerSymbolTickerStreamProcessor struct{}
-
-// ProcessData takes the data from Binance's 24hr symbol ticker stream and logs the watched symbol's data to the command line.
-func (processor *LoggerSymbolTickerStreamProcessor) ProcessData(data binance.SymbolTickerData) {
-	log.WithFields(log.Fields{
-		"data": data,
-	}).Info("Ticker for " + data.Symbol)
-}
-
-// processorFactory implements the SymbolStreamProcessorFactory contract which allows Coinfactory to
-// create processors for each symbol
-func processorFactory(symbol *coinfactory.Symbol) coinfactory.SymbolStreamProcessor {
-	proc := LoggerSymbolTickerStreamProcessor{}
-	return &proc
-}
-
 func main() {
-	cf := coinfactory.NewCoinFactory(processorFactory)
+	cf := coinfactory.NewCoinFactory()
+
+	// Make the kill switch for the various streams
+	killSwitch := make(chan bool)
+
+	// Create a list of markets to log
+	markets := []string{
+		"BTCUSDT",
+		"ETHUSDT",
+		"XRPUSDT",
+	}
+
+	// Create a processor for each ticker stream
+	for _, symbol := range markets {
+		go func(symbol string) {
+			log.Infof("Starting processor for %s market", symbol)
+			tickerStream := cf.GetTickerStreamService().GetTickerStream(killSwitch, symbol)
+
+			for {
+				// Bail on the kill switch
+				select {
+				case <-killSwitch:
+					return
+				default:
+				}
+
+				// Handle the data
+				select {
+				case data := <-tickerStream:
+					log.WithFields(log.Fields{
+						"data": data,
+					}).Info("Ticker for " + data.Symbol)
+
+				// Bail on the kill switch
+				case <-killSwitch:
+					return
+				}
+			}
+		}(symbol)
+
+	}
 	cf.Start()
 
 	// Intercept the interrupt signal and pass it along
@@ -36,6 +58,10 @@ func main() {
 
 	select {
 	case <-interrupt:
-		cf.Stop()
+		// Kill the streams
+		close(killSwitch)
+
+		// Shutdown coinfactory
+		cf.Shutdown()
 	}
 }
